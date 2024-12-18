2a5fa1da11d8aeb1a2b3795876e5800cf8e05a61
Shivam5672 <shivambansal.ind@gmail.com>
2024-12-18 15:37:20 +0530

    Adding Components and Pages Folders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>App</div>
+    
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+      </Routes> 
+    </Router>
+
   )
 }
 
